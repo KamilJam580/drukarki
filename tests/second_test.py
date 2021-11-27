d9e5a67f529d5f9e0491638f5a051f51a676c6a8
KamilJam580 <69052951+KamilJam580@users.noreply.github.com>
2021-11-27 19:55:27 +0100

    Test mock structure added

--- tests/second_test.py
+++ tests/second_test.py
@@ -1,9 +1,29 @@
+
+
+from requests.api import request
 import pytest
+import requests
+from unittest.mock import patch
+from functools import partial
+import sys
+import unittest
+
+class mock_request:
+    text = "0"
 
 
-class TestCalc:
+
+class TestCalc(unittest.TestCase):
     
-    def test_add_method(self):
+    def request_ricoh(self):
+        mockobj = mock_request()  
+        return mockobj
+  
+    @patch('requests.get', request_ricoh)
+    def test_ricoh_request(self):
+        response = requests.get(f"http:///web/guest/pl/websys/webArch/topPage.cgi").text
+        print(response)
 
-        assert 1 == 2
-        assert 3 == 3
+        
+if __name__ == '__main__':
+    unittest.main()
